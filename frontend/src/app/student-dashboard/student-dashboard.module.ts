7d20719d5bad38cb11953203c4732a393a3d8421
MajbriOussema <oussemamajbri@insat.u-carthage.tn>
2022-01-19 00:01:39 +0100

    AA

--- frontend/src/app/student-dashboard/student-dashboard.module.ts
+++ frontend/src/app/student-dashboard/student-dashboard.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnnouncementsComponent } from './announcements/announcements.component';
 import { SubjectsComponent } from './subjects/subjects.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
@@ -10,7 +9,6 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   declarations: [
     AnnouncementsComponent,
     SubjectsComponent,
-    SidebarComponent
   ],
   imports: [
     CommonModule
