5ba4178d646395ec0ae7b5a692e51be36ccb42ec
MajbriOussema <oussemamajbri@insat.u-carthage.tn>
2022-01-18 14:26:23 +0100

    added announcements and subjects

--- frontend/src/app/student-dashboard/student-dashboard.module.ts
+++ frontend/src/app/student-dashboard/student-dashboard.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { SubjectsComponent } from './subjects/subjects.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AnnouncementsComponent,
+    SubjectsComponent
+  ],
   imports: [
     CommonModule
   ]
